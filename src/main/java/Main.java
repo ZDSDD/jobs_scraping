cef88adebad04c6f94351d00a6e1c508eb092fbd
ZDSDD <106777224+ZDSDD@users.noreply.github.com>
2024-03-14 11:48:04 +0100

    delete comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,18 +7,13 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        // initializing the HTML Document page variable
         Document doc;
 
         try {
-            // fetching the target website
             doc = Jsoup.connect("https://www.pracuj.pl/praca").get();
-            // Select all elements with class "type--recommendation"
             Elements elements = doc.select("[data-test=offer-title]");
 
-            // Iterate through each element and extract the desired information
             for (Element element : elements) {
-                // Extract offer ID
                 System.out.println(element);
                 System.out.println("@@@@@@@@@@@@@@@@@@@@@@@\n");
             }
